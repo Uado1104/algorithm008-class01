863f38a807173be73bb3ad449a511a34ad637ab7
uado <18819446829@163.com>
2020-04-13 21:46:09 +0800

    Create moveZeroes.java
    2020/04/08

---
+++ Week_01/moveZeroes.java
@@ -0,0 +1,21 @@
+package Week_01;
+public class moveZeroes {
+    public void moveZeroes(int[] nums) {
+        int len = nums.length;
+        int next = 0;
+        if (len < 2) {
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != 0) {
+                nums[next] = nums[i];
+                next++;
+            }
+        }
+
+        for (int i = next; i < len; i++) {
+            nums[i] = 0;
+        }
+    }
+}
