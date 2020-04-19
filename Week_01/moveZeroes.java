b295386ca16f7d510bfa91ac88e685a23988c039
uado <18819446829@163.com>
2020-04-19 21:48:55 +0800

    Update moveZeroes.java

--- Week_01/moveZeroes.java
+++ Week_01/moveZeroes.java
@@ -19,3 +19,19 @@ public class moveZeroes {
         }
     }
 }
+
+class solution{
+    public void moveZeroes(int[] nums){
+        int next = 0;
+        for(int i =0 ;i<nums.length;i++){
+            if(nums[i] != 0 ){
+                nums[next] = nums[i];
+                if(i !=  next){
+                    nums[i] = 0;
+                
+                }
+                next++;
+            }
+        }
+    }
+}
