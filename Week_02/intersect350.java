c8bd05595807012c3405758a9d8a44d84d5f37fe
uado <18819446829@163.com>
2020-04-26 20:52:53 +0800

    Update intersect350.java

--- Week_02/intersect350.java
+++ Week_02/intersect350.java
@@ -3,26 +3,23 @@ package Week_02;
 import java.util.Arrays;
 import java.util.HashMap;
 
-java.util.HashMap;/*
-                  题目：给定两个数组，编写一个函数来计算它们的交集。
-                  示例 1:
-                  输入: nums1 = [1,2,2,1], nums2 = [2,2]
-                  输出: [2,2]
-                  示例 2:
-                  输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-                  输出: [4,9]
-                  说明：
-                  输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
-                  我们可以不考虑输出结果的顺序。
-                  
-                  */
+import java.util.HashMap;
+/*
+题目：给定两个数组，编写一个函数来计算它们的交集。
+示例 1:
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2,2]
+示例 2:
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [4,9]
+说明：
+输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+我们可以不考虑输出结果的顺序。
+*/
 
 public class intersect350 {
     private static void main(String args[]) {
-        int[] s = { 1, 2, 3, 4 };
-        int[] t = { 2, 3, 4 };
-        solution1 a = new intersect(s, t);
-        return a;
+
     }
 }
 
