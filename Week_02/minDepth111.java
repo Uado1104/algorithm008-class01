7f0af71acccfdf765df1a734fbc70ddca2f5223c
uado <18819446829@163.com>
2020-04-26 20:53:14 +0800

    Update minDepth111.java

--- Week_02/minDepth111.java
+++ Week_02/minDepth111.java
@@ -1,44 +1,81 @@
 package Week_02;
+
 import java.util.HashMap;
+import java.util.Queue;
+import java.util.LinkedList;
+
 /*
-题目：给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
-这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
-
-示例1：
-输入: pattern = "abba", str = "dog cat cat dog"
-输出: true
-
+题目：给定一个二叉树，找出其最小深度。
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+说明: 叶子节点是指没有子节点的节点。
+示例:
+给定二叉树 [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最小深度  2.
 */
-public class wordPattern290 {
+public class minDepth111 {
     private static void main(String args[]) {
-        String s = "1234";
-        String t = "2345";
-        solution1 a = new wordPattern(s,t) ;
-        return a;
     }
 }
 
-class solution1{
-    public boolean wordPattern(String pattern, String str) {
-        String[] array1 = str.split(" ");
-        if (array1.length != pattern.length()) {
-            return false;
-        }
-        String[] array2 = pattern.split("");
-        return wordPatternHelper(array1, array2) && wordPatternHelper(array2, array1);
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
     }
-    private boolean wordPatternHelper(String[] array1, String[] array2) {
-        HashMap<String, String> map = new HashMap<>(); 
-        for (int i = 0; i < array1.length; i++) {
-            String key = array1[i];
-            if (map.containsKey(key)) {
-                if (!map.get(key).equals(array2[i])) {
-                    return false;
+}
+
+// 通过递归来解决问题，深度优先搜索
+// 时间复杂度：O(N)，每个节点只访问一次。
+// 空间复杂度：O(N)
+/*
+ * 1.叶子节点的定义是左孩子和右孩子都为null时叫做叶子节点 2.当root节点左右孩子都为空时，返回1；
+ * 3.当root节点左右孩子有一个为空时，返回不为空的孩子节点的深度 4.点那个root节点左右孩子都不为空时，返回左右孩子较小深度的节点值
+ */
+class solution1 {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left_height = minDepth(root.left);
+        int right_height = minDepth(root.right);
+        return root.left == null || root.right == null ? left_height + right_height + 1
+                : Math.min(left_height, right_height) + 1;
+    }
+}
+
+// 思路：广度优先搜素
+class solution2 {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            depth++;
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                // 到达叶子节点
+                if (node.left == null && node.right == null) {
+                    return depth;
                 }
-            } else {
-                map.put(key, array2[i]);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
         }
-        return true;
+        return depth;
     }
 }
