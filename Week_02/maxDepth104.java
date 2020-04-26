683403b3d8cd74f65e45c003c0d9247db9200cbd
uado <18819446829@163.com>
2020-04-26 20:53:05 +0800

    Update maxDepth104.java

--- Week_02/maxDepth104.java
+++ Week_02/maxDepth104.java
@@ -1,44 +1,63 @@
 package Week_02;
+
 import java.util.HashMap;
 /*
-题目：给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
-这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
-
-示例1：
-输入: pattern = "abba", str = "dog cat cat dog"
-输出: true
-
+题目：给定一个二叉树，找出其最大深度。二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+说明: 叶子节点是指没有子节点的节点。
+示例：
+题目：给定二叉树 [3,9,20,null,null,15,7]，
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
 */
-public class wordPattern290 {
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+public class maxDepth104 {
     private static void main(String args[]) {
-        String s = "1234";
-        String t = "2345";
-        solution1 a = new wordPattern(s,t) ;
-        return a;
+
     }
 }
 
-class solution1{
-    public boolean wordPattern(String pattern, String str) {
-        String[] array1 = str.split(" ");
-        if (array1.length != pattern.length()) {
-            return false;
-        }
-        String[] array2 = pattern.split("");
-        return wordPatternHelper(array1, array2) && wordPatternHelper(array2, array1);
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
     }
-    private boolean wordPatternHelper(String[] array1, String[] array2) {
-        HashMap<String, String> map = new HashMap<>(); 
-        for (int i = 0; i < array1.length; i++) {
-            String key = array1[i];
-            if (map.containsKey(key)) {
-                if (!map.get(key).equals(array2[i])) {
-                    return false;
-                }
-            } else {
-                map.put(key, array2[i]);
-            }
+}
+
+// 通过递归来解决问题，深度优先搜索
+// 时间复杂度：O(N)，每个节点只访问一次。
+// 空间复杂度：O(N)
+class solution1 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
         }
-        return true;
+    }
+}
+
+// 思路：递归调用
+class solution2 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
+        }
     }
 }
